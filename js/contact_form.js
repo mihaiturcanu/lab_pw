652305a9596b14c46a238588eb5e6ca1db70d205
Mihai Turcanu <mihai.turcanu05@gmail.com>
2026-02-25 16:18:46 +0200

    Lab 1: HTML and CSS review

---
+++ js/contact_form.js
@@ -0,0 +1,9 @@
+function submitForm(){
+    var nume = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    console.log(nume);
+    console.log(email);
+    console.log(message);
+    console.warn("Goodbye, World!");
+}
