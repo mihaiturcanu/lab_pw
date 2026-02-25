222f9f1d5c488dab26de16c2afaa6328c9e8a916
mihaiturcanu <mihai.turcanu05@gmail.com>
2026-02-25 17:19:03 +0200

    feat: adauga salut personalizat bazat pe ora zilei

--- js/contact_form.js
+++ js/contact_form.js
@@ -7,3 +7,18 @@ function submitForm(){
     console.log(message);
     console.warn("Goodbye, World!");
 }
+
+function greeting(){
+    date = new Date().getHours();
+    if(date>=6 && date <=11){
+        document.querySelector('header p').textContent = "Bună dimineața! Bine ai venit pe pagina mea.";
+    }
+    else if(date>=12 && date<=17){
+        document.querySelector('header p').textContent = "Bună ziua! Bine ai venit pe pagina mea.";
+    }
+    else{
+        document.querySelector('header p').textContent = "Bună seara! Bine ai venit pe pagina mea.";
+    }
+}
+
+greeting();
