b12fbe7b686d6922345e47eab964d5ac0a310a80
mihaiturcanu <mihai.turcanu05@gmail.com>
2026-03-11 17:01:45 +0200

    feat: add cards from JS array

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import Card from './Card';
 
-function App() {     
+function App() {  
+  const projects = [
+    {title:"Proiect 1", description:"Pagina personala"},
+    {title:"Proiect 2", description:"Calculator buget"},
+    {title:"Proiect 3", description:"Dashboard React"},
+    {title:"Proiect 4", description:"Password Generator"},
+    {title:"Proiect 5", description:"Personal webpage"}
+  ];   
   return (         
   <div>             
     <h1>Dashboard</h1>             
     <p>Mihai Turcanu </p>
     <p>Student Calculatoare, anul II</p>
-    <Card title="Proiect 1" description="Pagina personala cu HTML si CSS" />
-    <Card title="Proiect 2" description="Pagina interactiva cu Javascript" />
-    <Card title="Proiect 3" description="Dashboard cu React" />    
+    {projects.map(function(item, index){
+      return <Card key={index} title={item.title} description={item.description} />
+    })}
     </div>     
     ); 
   }  
