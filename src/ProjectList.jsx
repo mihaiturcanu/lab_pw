8e48adb2c7a09b1774cfb6d004f97c2b767ccde9
mihaiturcanu <mihai.turcanu05@gmail.com>
2026-03-25 17:06:00 +0200

    feat: add error checking in case projects.json is not found

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -4,17 +4,22 @@ import Card from './Card';
 function ProjectList(){
     const[projects, setProjects] = useState([]);
     const[loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
     useEffect(function(){
         fetch('/data/projects.json').then(function(response){
             return response.json();
         }).then(function(data){
             setProjects(data.projects);
             setLoading(false);
+        }).catch(function(err){
+            setError('Eroare la incarcarea datelor ' + err);
+            setLoading(false);
         });
     }, []);
     if(loading){
         return <p>Se incarca...</p>
     }
+    if(error) return <p>{error}</p>
     return(
         <div>
             <h3>Proiecte</h3>
