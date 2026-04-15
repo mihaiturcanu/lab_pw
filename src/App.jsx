c4bb5d42fadea0ecf49d697009f56e455a3b0e6d
mihaiturcanu <mihai.turcanu05@gmail.com>
2026-04-15 16:30:55 +0300

    feat: add NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from "./pages/Contact";
 import Navbar from "./Navbar";
+import NotFound from "./pages/NotFound";
 
 function App(){
   return(
@@ -12,6 +13,7 @@ function App(){
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<Projects/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+function NotFound(){
+    return(
+        <h1>404 - Pagina nu exista</h1>
+    );
+}
+
+export default NotFound;
