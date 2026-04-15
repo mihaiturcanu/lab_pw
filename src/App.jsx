87a550fab16713147a16ce44bcb9383229dac6f8
mihaiturcanu <mihai.turcanu05@gmail.com>
2026-04-15 16:22:34 +0300

    feat: add a Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from "./pages/Contact";
+import Navbar from "./Navbar";
 
 function App(){
   return(
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<Projects/>}/>
