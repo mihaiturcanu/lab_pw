37697764552bd764108567e98198643ed173ba65
mihaiturcanu <mihai.turcanu05@gmail.com>
2026-04-15 16:37:01 +0300

    feat: add About page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Projects from './pages/Projects';
 import Contact from "./pages/Contact";
 import Navbar from "./Navbar";
 import NotFound from "./pages/NotFound";
+import About from "../About";
 
 function App(){
   return(
@@ -14,6 +15,7 @@ function App(){
         <Route path="/projects" element={<Projects/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="*" element={<NotFound/>}/>
+        <Route path="/about" element={<About/>}/>
       </Routes>
     </BrowserRouter>
   );
