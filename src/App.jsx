9a2b1e1ccc67ee9f9db35be50b9b4dc4b881921f
mihaiturcanu <mihai.turcanu05@gmail.com>
2026-04-15 16:17:06 +0300

    feat: refactor the website to use Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,18 @@
-import Card from './Card';
-import QuickNote from './QuickNote';
-import TodoList from './TodoList';
-import ContactForm from './ContactForm';
-import Clock from './Clock';
-import { useState } from 'react';
-import ProjectList from './ProjectList';
-import LoadApiData from './LoadApiData';
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from "./pages/Contact";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const projects = [
-    {title:"Proiect 1", description:"Pagina personala"},
-    {title:"Proiect 2", description:"Calculator buget"},
-    {title:"Proiect 3", description:"Dashboard React"},
-    {title:"Proiect 4", description:"Password Generator"},
-    {title:"Proiect 5", description:"Personal webpage"}
-  ];   
-  return (         
-  <div>
-    <Clock/>
-    <LoadApiData/>             
-    <h1>Dashboard</h1>             
-    <p>Mihai Turcanu </p>
-    <p>Student Calculatoare, anul II</p>
-    <p>Ai apasat de {count} ori</p>
-    <button onClick={() => setCount(count + 1)}>Click</button>
-    <button onClick={() => setCount(count - 1)}>Decrease counter</button>
-    <button onClick={() => setCount(0)}>Reset counter</button>
-    {projects.map(function(item, index){
-      return <Card key={index} title={item.title} description={item.description} />
-    })}
-    <ProjectList/>
-    <QuickNote/>
-    <TodoList/>
-    <ContactForm/>
-    </div>     
-    ); 
-  }  
-  export default App; 
+function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <div>
+            <h2>Home</h2>
+            <p>Bine ai venit pe dashboard-ul meu!</p>
+        </div>
+    );
+}
+
+export default Home;
