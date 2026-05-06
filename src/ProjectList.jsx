9d74005889190b70ec97b457fdfcdd74f0e995a1
mihaiturcanu <mihai.turcanu05@gmail.com>
2026-05-06 14:07:40 +0300

    feat: fetch from React to API

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -7,10 +7,10 @@ function ProjectList(){
     const[error, setError] = useState(null);
     const[search, setSearch] = useState('');
     useEffect(function(){
-        fetch('/data/projects.json').then(function(response){
+        fetch('http://localhost:3000/api/projects').then(function(response){
             return response.json();
         }).then(function(data){
-            setProjects(data.projects);
+            setProjects(data);
             setLoading(false);
         }).catch(function(err){
             setError('Eroare la incarcarea datelor ' + err);
