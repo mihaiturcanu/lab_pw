e0d41453848f4ffc609d47907e77aecde221517a
mihaiturcanu <mihai.turcanu05@gmail.com>
2026-05-06 13:56:56 +0300

    activate CORS on server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors'); 
+app.use(cors()); 
+
 const mongoose = require('mongoose'); 
 const Project = require('./models/Project');
 
